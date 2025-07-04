0d2a43db99c6f7998e1e4e0060a2b74925969b6a
regularuser0 <hdelarue@proton.me>
2025-07-05 01:00:44 +0200

    radius

--- script/tidle.sh
+++ script/tidle.sh
@@ -4,8 +4,8 @@ running=$(ps aux | grep '[h]ypridle')
 
 if [ -n "$running" ]; then
     pkill hypridle
-    echo "hypridle off" > ~/.config/script/idlestate
+    echo "off" > ~/.config/script/idlestate
 else
     hyprctl dispatch exec hypridle
-    echo "hypridle on" > ~/.config/script/idlestate
+    echo "on" > ~/.config/script/idlestate
 fi
